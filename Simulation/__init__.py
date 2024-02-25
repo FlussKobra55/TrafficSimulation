4afdad6ae0be481bd55e5ac1e2699b958f220ed1
FlussKobra55 <julian.steinacker@gmail.com>
2024-02-25 22:10:00 +0100

    initialsed the dict

---
+++ Simulation/__init__.py
@@ -0,0 +1,9 @@
+from .Geometry.Segment import Segment
+from .Geometry.QuadraticCurve import QuadraticCurve
+from .Geometry.CubicCurve import CubicCurve
+
+from .Vehicle import Vehicle
+from .VehicleGenerator import VehicleGenerator
+
+from .Simulation import Simulation
+from .window import Window
